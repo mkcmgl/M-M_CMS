5a6ad6c8405d33219243c16bbcc79b237dbcb260
mkcmgl <2515318247@qq.com>
2023-03-02 17:15:12 +0800

    first

---
+++ src/components/admin/dashboard/admin/components/PanelGroup.vue
@@ -0,0 +1,178 @@
+<template>
+  <el-row :gutter="40" class="panel-group">
+    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+      <div class="card-panel" @click="handleSetLineChartData('newVisitis')">
+        <div class="card-panel-icon-wrapper icon-people">
+          <i class="el-icon-user-solid" style="font-size: 50px"></i>
+        </div>
+        <div class="card-panel-description">
+          <div class="card-panel-text">
+            New Visits
+          </div>
+          <count-to :start-val="0" :end-val="3734" :duration="2600" class="card-panel-num" />
+        </div>
+      </div>
+    </el-col>
+    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+      <div class="card-panel" @click="handleSetLineChartData('messages')">
+        <div class="card-panel-icon-wrapper icon-message">
+          <i class="el-icon-message" style="font-size: 50px"></i>
+        </div>
+        <div class="card-panel-description">
+          <div class="card-panel-text">
+            Messages
+          </div>
+          <count-to :start-val="0" :end-val="676" :duration="3000" class="card-panel-num" />
+        </div>
+      </div>
+    </el-col>
+    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+      <div class="card-panel" @click="handleSetLineChartData('purchases')">
+        <div class="card-panel-icon-wrapper icon-money">
+          <i class="el-icon-star-on" style="font-size: 50px"></i>
+        </div>
+        <div class="card-panel-description">
+          <div class="card-panel-text">
+            Stars
+          </div>
+          <count-to :start-val="0" :end-val="292" :duration="3200" class="card-panel-num" />
+        </div>
+      </div>
+    </el-col>
+    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+      <div class="card-panel" @click="handleSetLineChartData('shoppings')">
+        <div class="card-panel-icon-wrapper icon-shopping">
+          <i class="el-icon-money" style="font-size: 50px"></i>
+        </div>
+        <div class="card-panel-description">
+          <div class="card-panel-text">
+            Donations
+          </div>
+          <count-to :start-val="0" :end-val="7690" :duration="3600" class="card-panel-num" />
+        </div>
+      </div>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import CountTo from 'vue-count-to'
+
+export default {
+  components: {
+    CountTo
+  },
+  methods: {
+    handleSetLineChartData (type) {
+      this.$emit('handleSetLineChartData', type)
+    }
+  }
+}
+</script>
+
+<style  scoped>
+.panel-group {
+  margin-top: 18px;
+}
+
+.panel-group .card-panel-col {
+  margin-bottom: 32px;
+}
+
+.panel-group .card-panel {
+  height: 108px;
+  cursor: pointer;
+  font-size: 12px;
+  position: relative;
+  overflow: hidden;
+  color: #666;
+  background: #fff;
+  box-shadow: 4px 4px 40px rgba(0, 0, 0, 0.05);
+  border-color: rgba(0, 0, 0, 0.05);
+}
+
+.panel-group .card-panel:hover .card-panel-icon-wrapper {
+  color: #fff;
+}
+
+.panel-group .card-panel:hover .icon-people {
+  background: #40c9c6;
+}
+
+.panel-group .card-panel:hover .icon-message {
+  background: #36a3f7;
+}
+
+.panel-group .card-panel:hover .icon-money {
+  background: #f4516c;
+}
+
+.panel-group .card-panel:hover .icon-shopping {
+  background: #34bfa3;
+}
+
+.panel-group .card-panel .icon-people {
+  color: #40c9c6;
+}
+
+.panel-group .card-panel .icon-message {
+  color: #36a3f7;
+}
+
+.panel-group .card-panel .icon-money {
+  color: #f4516c;
+}
+
+.panel-group .card-panel .icon-shopping {
+  color: #34bfa3;
+}
+
+.panel-group .card-panel .card-panel-icon-wrapper {
+  float: left;
+  margin: 14px 0 0 14px;
+  padding: 16px;
+  transition: all 0.38s ease-out;
+  border-radius: 6px;
+}
+
+.panel-group .card-panel .card-panel-icon {
+  float: left;
+  font-size: 48px;
+}
+
+.panel-group .card-panel .card-panel-description {
+  float: right;
+  font-weight: bold;
+  margin: 26px;
+  margin-left: 0px;
+}
+
+.panel-group .card-panel .card-panel-description .card-panel-text {
+  line-height: 18px;
+  color: rgba(0, 0, 0, 0.45);
+  font-size: 16px;
+  margin-bottom: 12px;
+}
+
+.panel-group .card-panel .card-panel-description .card-panel-num {
+  font-size: 20px;
+}
+
+@media (max-width: 550px) {
+  .card-panel-description {
+    display: none;
+  }
+  .card-panel-icon-wrapper {
+    float: none !important;
+    width: 100%;
+    height: 100%;
+    margin: 0 !important;
+  }
+  .card-panel-icon-wrapper .svg-icon {
+    display: block;
+    margin: 14px auto !important;
+    float: none !important;
+  }
+}
+
+</style>
